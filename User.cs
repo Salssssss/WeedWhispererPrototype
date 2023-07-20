183ca2212260397e93ecf0905b7b650df12c564d
Liam Ison <jison5@students.kennesaw.edu>
2023-07-20 16:58:04 -0400

    added framwork to UserDAO and RegisterUser

--- User.cs
+++ User.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-internal class User
+public class User
 {
 
     public int Id { get; set; }
--- UserDAO.cs
+++ UserDAO.cs
@@ -3,8 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System;
+using System.Data;
+using System.Data.SqlClient;
+
 public class UserDAO
 {
-    
+    private readonly string connectionString;
+
+    public UserDAO(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    public User GetUserById(int id)
+    {
+        
+    }
+
+    public User GetUserByEmail(string email)
+    {
+       
+    }
+
+    public void AddUser(User user)
+    {
+        
+    }
+
+    public void UpdateUser(User user)
+    {
+        
+    }
+
+    public void DeleteUser(int id)
+    {
+        
+    }
 }
