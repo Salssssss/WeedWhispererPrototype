d61eb82a31d77de979ba48a59daead50e5e6d5ca
luis~ <94722453+Luis-avalos1@users.noreply.github.com>
2023-07-24 14:17:19 -0700

    Update PlantRecomendation.cs

--- PlantRecomendation.cs
+++ PlantRecomendation.cs
@@ -5,5 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 public class PlantRecommendation
 {
+  private PlantDAO plantDAO;
+
+  public PlantRecommendation(string connectionString)
+  { 
+    plantDAO = new PlantDAO(connectionString);
+  }
+
+  public List<Plant> getRecommendedPlants(string nativeStatus = null, string growthHabit = null, string plantGroup = null, string duration = null)
+  {
+
+    List<Plant> recommended =
+  }
+    
+
+  
 
 }
