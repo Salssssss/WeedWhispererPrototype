a80f9a6e80093f04b3d2969703307f1efde30af3
Liam Ison <jison5@students.kennesaw.edu>
2023-07-23 20:02:34 -0400

    WaterSChedule and ScheduleDAO

--- ScheduleDAO.cs
+++ ScheduleDAO.cs
@@ -1,10 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 public class ScheduleDAO
 {
+    private readonly string connectionString;
 
+    public ScheduleDAO(string connectionString)
+    {
+        this.connectionString = connectionString;
+    }
+
+    public void AddWaterSchedule(WaterSchedule schedule)
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            string query = "INSERT INTO WaterSchedules (PlantId, WateringDate) VALUES (@PlantId, @WateringDate)";
+            SqlCommand command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("@PlantId", schedule.PlantId);
+            command.Parameters.AddWithValue("@WateringDate", schedule.WateringDate);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+        }
+    }
+
+    public List<WaterSchedule> GetWaterSchedules()
+    {
+        List<WaterSchedule> schedules = new List<WaterSchedule>();
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            string query = "SELECT Id, PlantId, WateringDate FROM WaterSchedules";
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                WaterSchedule schedule = new WaterSchedule
+                {
+                    Id = (int)reader["Id"],
+                    PlantId = (int)reader["PlantId"],
+                    WateringDate = (DateTime)reader["WateringDate"]
+                };
+
+                schedules.Add(schedule);
+            }
+        }
+
+        return schedules;
+    }
 }
---
+++ WaterSchedule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class WaterSchedule
+{
+    public int Id { get; set; }
+    public int PlantId { get; set; }
+    public DateTime WateringDate { get; set; }
+
+    public WaterSchedule()
+    {
+        // Default constructor
+    }
+
+    public WaterSchedule(int id, int plantId, DateTime wateringDate)
+    {
+        Id = id;
+        PlantId = plantId;
+        WateringDate = wateringDate;
+    }
+}
