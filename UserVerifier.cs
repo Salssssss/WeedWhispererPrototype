965b1addfe165ceb3b82706d6455b0409d570a85
andres <andresfelipec43@gmail.com>
2023-07-24 12:25:14 -0400

    userverifier

--- UserVerifier.cs
+++ UserVerifier.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 public class UserVerifier
 {
+
+    User user = new User();
     public bool VerifyUserCredentials(string email, string password)
     {
         // Check if the provided email and password match the user's credentials in the database
         // Return true if the credentials are valid, false otherwise
+        
+        if (email == user.Email && password == user.Password) { return true; }
+
+        else { return false; }
     }
 }
