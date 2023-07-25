9694e14dc0ba0f701ed5a4db9360ed831ef163aa
tnsmi <140455633+tnsmi@users.noreply.github.com>
2023-07-24 21:34:13 -0400

    Update MyPlants.cs

--- MyPlants.cs
+++ MyPlants.cs
@@ -15,16 +15,44 @@ public class MyPlants
     public void AddPlant(Plant plant)
     {
         // Add plant to the list
+        if (!string.IsNullOrWhiteSpace(plant))
+        {
+            Plants.add(plant);
+            Console.WriteLine(plant + " has been added to your list of plants!");
+        }
+        else
+        {
+            Console.WriteLine("Plant name cannot be empty, please add the name of your plant.");
+        }   
+            
     }
 
     public void RemovePlant(Plant plant)
     {
         // Remove plant from the list
+        if (Plants.contains(plant))
+        {
+            Plant.remove(plant);
+            Console.WriteLine(plant + " has been removed from your plnats.");
+        }
+        else
+        {
+            Console.WriteLine("Plant not found.");
+        }
+        
     }
 
     public List<Plant> GetPlants()
     {
         // Return the list of plants
-        return Plants;
+        if (Plants.count > 0)
+        {
+            return Plants;
+        }
+        else
+        {
+            Console.WriteLine("Your list is empty.");
+        }
+        
     }
 }
