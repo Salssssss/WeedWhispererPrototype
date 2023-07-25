ba520d8b6d66db9de874447eaf93bbbe08ad5df4
Liam Ison <jison5@students.kennesaw.edu>
2023-07-25 16:47:16 -0400

    ScheduleDAO and WaterSchedule working

--- ScheduleDAO.cs
+++ ScheduleDAO.cs
@@ -1,56 +1,147 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.SqlClient;
 
 public class ScheduleDAO
 {
-    private readonly string connectionString;
+    private string connectionString;
 
     public ScheduleDAO()
     {
         this.connectionString = "Server=tcp:weedwhisperer.database.windows.net,1433;Initial Catalog=weedwhisperer;Persist Security Info=False;User ID=weeder;Password=Whisper123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
     }
 
-    public void AddWaterSchedule(WaterSchedule schedule)
+    public void AddWaterSchedule(WaterSchedule waterSchedule)
     {
-        using (SqlConnection connection = new SqlConnection(connectionString))
+        try
         {
-            string query = "INSERT INTO WaterSchedules (PlantId, WateringDate) VALUES (@PlantId, @WateringDate)";
-            SqlCommand command = new SqlCommand(query, connection);
-            command.Parameters.AddWithValue("@PlantId", schedule.PlantId);
-            command.Parameters.AddWithValue("@WateringDate", schedule.WateringDate);
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                waterSchedule.Id = GenerateUniqueScheduleId();
+                connection.Open();
 
-            connection.Open();
-            command.ExecuteNonQuery();
+                string query = "INSERT INTO Schedule (ScheduleId, UserId, PlantId, WateringDate) " +
+                               "VALUES (@ScheduleId, @UserId, @PlantId, @WateringDate)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ScheduleId", waterSchedule.Id);
+                    command.Parameters.AddWithValue("@UserId", waterSchedule.UserId);
+                    command.Parameters.AddWithValue("@PlantId", waterSchedule.PlantId);
+                    command.Parameters.AddWithValue("@WateringDate", waterSchedule.WateringDate);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        Console.WriteLine("Watering schedule added successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Failed to add watering schedule.");
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred while adding watering schedule: " + ex.Message);
         }
     }
 
-    public List<WaterSchedule> GetWaterSchedules()
+    public void RemoveWaterSchedule(int scheduleId)
     {
-        List<WaterSchedule> schedules = new List<WaterSchedule>();
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
 
+                string query = "DELETE FROM Schedule WHERE ScheduleId = @ScheduleId";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@ScheduleId", scheduleId);
+
+                    int rowsAffected = command.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                    {
+                        Console.WriteLine($"Watering schedule with ScheduleId {scheduleId} removed successfully.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Watering schedule with ScheduleId {scheduleId} not found.");
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred while removing watering schedule: " + ex.Message);
+        }
+    }
+    public List<WaterSchedule> GetWaterSchedulesByUserId(int userId)
+    {
+        List<WaterSchedule> waterSchedules = new List<WaterSchedule>();
+
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT ScheduleId, UserId, PlantId, WateringDate " +
+                               "FROM Schedule " +
+                               "WHERE UserId = @UserId";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@UserId", userId);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int scheduleId = (int)reader["ScheduleId"];
+                            int plantId = (int)reader["PlantId"];
+                            DateTime wateringDate = (DateTime)reader["WateringDate"];
+
+                            WaterSchedule waterSchedule = new WaterSchedule(scheduleId, userId, plantId, wateringDate);
+                            waterSchedules.Add(waterSchedule);
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred while fetching watering schedules: " + ex.Message);
+        }
+
+        return waterSchedules;
+    }
+    private int GenerateUniqueScheduleId()
+    {
         using (SqlConnection connection = new SqlConnection(connectionString))
         {
-            string query = "SELECT Id, PlantId, WateringDate FROM WaterSchedules";
+            string query = "SELECT MAX(ScheduleId) FROM Schedule";
             SqlCommand command = new SqlCommand(query, connection);
 
             connection.Open();
-            SqlDataReader reader = command.ExecuteReader();
+            object result = command.ExecuteScalar();
 
-            while (reader.Read())
+            if (result == null || result == DBNull.Value)
             {
-                WaterSchedule schedule = new WaterSchedule
-                {
-                    Id = (int)reader["Id"],
-                    PlantId = (int)reader["PlantId"],
-                    WateringDate = (DateTime)reader["WateringDate"]
-                };
-
-                schedules.Add(schedule);
+                // If the table is empty, start with ID 1
+                return 1;
+            }
+            else
+            {
+                // Otherwise, generate the next unique ID
+                int maxId = Convert.ToInt32(result);
+                return maxId + 1;
             }
         }
-
-        return schedules;
     }
 }
--- WaterSchedule.cs
+++ WaterSchedule.cs
@@ -3,18 +3,43 @@
 public class WaterSchedule
 {
     public int Id { get; set; }
+    public int UserId { get; set; }
     public int PlantId { get; set; }
     public DateTime WateringDate { get; set; }
 
-    public WaterSchedule()
+    public WaterSchedule(int scheduleId, int userId, int plantId, DateTime wateringDate)
     {
-        // Default constructor
+        Id = scheduleId;
+        UserId = userId;
+        PlantId = plantId;
+        WateringDate = wateringDate.Date;
+    }
+    public WaterSchedule(int userId, int plantId, DateTime wateringDate)
+    {
+        UserId = userId;
+        PlantId = plantId;
+        WateringDate = wateringDate.Date;
     }
 
-    public WaterSchedule(int id, int plantId, DateTime wateringDate)
+    public void SaveWateringSchedule()
     {
-        Id = id;
-        PlantId = plantId;
-        WateringDate = wateringDate;
+        ScheduleDAO scheduleDAO = new ScheduleDAO();
+        scheduleDAO.AddWaterSchedule(this);
+    }
+
+    public void AlertUserIfWateringDayHasArrived()
+    {
+        // Get the current date and time
+        DateTime currentDate = DateTime.Now;
+
+        // Check if the watering date has arrived
+        if (currentDate.Date >= WateringDate.Date)
+        {
+            // Send an alert to the user (however we do that)
+            Console.WriteLine($"Alert: Today is the watering day for PlantId {PlantId}!");
+
+            ScheduleDAO scheduleDAO = new ScheduleDAO();
+            scheduleDAO.RemoveWaterSchedule(Id);
+        }
     }
 }
