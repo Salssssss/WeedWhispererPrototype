4a56d259ed9547a57d027f0a30ced0e244277b91
luis~ <94722453+Luis-avalos1@users.noreply.github.com>
2023-07-26 18:53:27 -0700

    Update UserVerifier.cs
    Added error handling and made it so UserDAO is instantiated only once instead of instantiating a new userDAO object each time "UserVerifier" is called.

--- UserVerifier.cs
+++ UserVerifier.cs
@@ -6,19 +6,29 @@ using System.Text;
 using System.Threading.Tasks;
 public class UserVerifier
 {
+    private readonly UserDAO userDAO;
 
+    public UserVerifier(){
+        this.userDAO = new UserDAO();
+    }
+    
     public bool VerifyUserCredentials(string email, string password)
     {
-        UserDAO verify_user = new UserDAO();
-        User user = verify_user.GetUserByEmail(email);
+        try{
 
-        if (user != null && email == user.Email && password == user.Password)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
+            User user = verify_user.GetUserByEmail(email);
+
+            if (user != null && email == user.Email && password == user.Password)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            }catch (Exception e){
+                Console.WriteLine(e.Message);
+                return false;
         }
     }
 
